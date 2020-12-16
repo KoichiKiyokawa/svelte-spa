655396a0235e8a38cd40d03e1e188ba3df539bff
K.Kiyokawa <koichi20110068@gmail.com>
2020-12-16 18:45:08 +0900

    feat: use withAuth HOC

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,18 @@
 import { firebase } from '@/plugins/firebase'
+import type { SvelteComponent } from 'svelte'
 import { push } from 'svelte-spa-router'
 import { wrap } from 'svelte-spa-router/wrap'
+
 import Dashboard from './routes/index.svelte'
 import Login from './routes/login.svelte'
 
 export const routes = {
-  '/': wrap({ component: Dashboard, conditions: [redirectIfNotLoggedIn] }),
+  '/': withAuth(Dashboard),
   '/login': Login,
+}
+
+function withAuth(component: typeof SvelteComponent) {
+  return wrap({ component, conditions: [redirectIfNotLoggedIn] })
 }
 
 async function redirectIfNotLoggedIn() {
@@ -14,7 +20,7 @@ async function redirectIfNotLoggedIn() {
     await new Promise((resolve, reject) => {
       firebase.auth().onAuthStateChanged((user) => {
         if (!user) reject()
-        resolve()
+        resolve(user)
       })
     })
     return true
